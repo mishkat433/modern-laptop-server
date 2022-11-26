5d06e3e091e3b55ddc371383494f50bafb061324
miskat433 <miskat433@gmail.com>
2022-11-27 01:34:21 +0600

    fix

--- index.js
+++ index.js
@@ -10,12 +10,29 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET);
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.twfgu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 app.get('/', (req, res) => {
     res.send("server is running")
 })
 
-const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.twfgu.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+function veryfyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unAuthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.JSON_SECRET_KEY, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
+
 
 
 async function run() {
@@ -26,13 +43,12 @@ async function run() {
         const bookingCollection = client.db("modernLaptop").collection("booking");
         const paymentCollection = client.db("modernLaptop").collection("payments");
 
-
-        app.get('/jwt', async (req, res) => {
-            const email = req.query.email;
+        app.post('/jwt', async (req, res) => {
+            const email = req.body.email;
             const query = { email: email }
             const user = await userCollection.find(query)
             if (user) {
-                const token = jwt.sign({ email }, process.env.JSON_SECRET_KEY, { expiresIn: '1d' })
+                const token = jwt.sign({ email }, process.env.JSON_SECRET_KEY, { expiresIn: '2d' })
                 return res.send({ accessToken: token })
             }
             res.status(403).send({ accessToken: "Forbidden Access" })
@@ -67,8 +83,12 @@ async function run() {
             });
         })
 
-        app.post('/finalPayment', async (req, res) => {
+        app.post('/finalPayment', veryfyJWT, async (req, res) => {
             const payment = req.body;
+            const jwtEmail = req.query.email;
+            if (req.decoded.email !== jwtEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             const result = await paymentCollection.insertOne(payment)
             const id = payment.bookingId;
             const query = { _id: ObjectId(id) }
@@ -108,9 +128,13 @@ async function run() {
             return res.send(arr)
         })
 
-        app.get('/myProduct/:email', async (req, res) => {
+        app.get('/myProduct/:email', veryfyJWT, async (req, res) => {
             const findEmail = req.params.email;
             const query = {}
+            const jwtEmail = req.query.email;
+            if (req.decoded.email !== jwtEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             const result = await productCollection.find(query).sort({ _id: -1 }).toArray()
             const arr = []
             result.forEach(data => {
@@ -133,14 +157,22 @@ async function run() {
             return res.send(adItem)
         })
 
-        app.post('/addProduct', async (req, res) => {
+        app.post('/addProduct', veryfyJWT, async (req, res) => {
             const data = req.body
+            const jwtEmail = req.query.email;
+            if (req.decoded.email !== jwtEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             const cursor = await productCollection.insertOne(data)
             res.send(cursor)
         })
 
-        app.put('/productAdvertise/:id', async (req, res) => {
+        app.put('/productAdvertise/:id', veryfyJWT, async (req, res) => {
             const id = req.params.id
+            const jwtEmail = req.query.email;
+            if (req.decoded.email !== jwtEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             const filter = { _id: ObjectId(id) }
             const options = { upsert: true }
             const addAdvertise = {
@@ -163,9 +195,12 @@ async function run() {
 
         //------------------------------ booking Section ----------------------------
 
-        app.get('/myBooking/:email', async (req, res) => {
+        app.get('/myBooking/:email', veryfyJWT, async (req, res) => {
             const findEmail = req.params.email;
-
+            const jwtEmail = req.query.email;
+            if (req.decoded.email !== jwtEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             const query = {}
             const result = await bookingCollection.find({}).toArray()
             const arr = []
@@ -208,9 +243,13 @@ async function run() {
             return res.send(result)
         })
 
-        app.get('/users/check/:email', async (req, res) => {
+        app.get('/users/check/:email', veryfyJWT, async (req, res) => {
             const email = req.params.email;
             const query = { email: email }
+            const jwtEmail = req.query.email;
+            if (req.decoded.email !== jwtEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             const user = await userCollection.findOne(query)
             if (user?.userType === 'user') {
                 return res.send({ useCheck: user?.userType })
